c48cf06421144aaec1625e445eff113cf222ac48
SidneySil <69279632+sidneysil@users.noreply.github.com>
2021-07-11 20:20:49 -0300

    CRUD flutter

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+      name: 'Sidney',
+      email: 'sidneysil01@gmail.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_960_720.png'),
+  '2': const User(
+      name: 'Gustavo',
+      email: 'gustavo@gmail.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2021/06/18/07/19/man-6345374_960_720.png'),
+  '3': const User(
+      name: 'mariana',
+      email: 'mariana@gmail.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2021/06/07/06/42/man-6317130_960_720.png'),
+};
